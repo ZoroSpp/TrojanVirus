20fa523baf2b97f2c198b2ef938dd1417e0466b5
Sura Prasad Pradhan <surapp19@gmail.com>
2021-09-17 13:21:56 +0530

    Background+movement

--- Trojan Virus/Assets/Scripts/Spark.cs
+++ Trojan Virus/Assets/Scripts/Spark.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spark : MonoBehaviour
@@ -7,9 +5,17 @@ public class Spark : MonoBehaviour
     [SerializeField] Transform pos;
     [SerializeField] float speed;
     Vector3 targetpos;
+    float Sparktime;                    // start of sparks runtime
+    float timelapse;                    // timeperiod of each cycle
+    [SerializeField] float freezeTime;
+    float freezeStart;
+    int checkSt = 0;                    //running status of spark
+    
     // Start is called before the first frame update
     void Start()
     {
+        Sparktime = Time.time;
+        timelapse = Random.Range(4, 10)*0.2f;
         TargetPos();
     }
 
@@ -19,12 +25,36 @@ public class Spark : MonoBehaviour
         if(pos.position != targetpos)
         {
             pos.position=Vector3.MoveTowards(transform.position,targetpos,speed*Time.deltaTime);
-
         }
 
         else
         {
-            TargetPos();
+            if (Time.time >= Sparktime + timelapse)
+            {
+                
+                switch (checkSt)
+                {
+                    case 0:
+                        freezeStart = Time.time;
+                        checkSt = 1;
+                        speed = 0;
+                        break;
+                    case 1:
+                        if (Time.time >= freezeStart + freezeTime)
+                        {
+                            Sparktime = Time.time;
+                            timelapse = Random.Range(4, 10) * 0.2f;
+                            speed = 40;
+                            checkSt= 0;
+                            TargetPos();
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                TargetPos();
+            }
         }
 
     }
@@ -33,6 +63,5 @@ public class Spark : MonoBehaviour
         targetpos.x = (Random.Range(0,3)-1)*5.2f;
         targetpos.y = (Random.Range(0,3)-1)*5.2f;
         targetpos.z = 0.0f;
-        Debug.Log(targetpos);
-    }
+        }
 }
